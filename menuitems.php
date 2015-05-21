6f70f8696822ea1c457de9fb1edafd6730d0777f
Axel <axel.hawker@gmail.com>
2015-05-21 11:11:44 -0500

    Added Logout to Menu
    Created logout.php and linked to it on Menu

--- menuitems.php
+++ menuitems.php
@@ -2,5 +2,5 @@
                 <li class="pure-menu-item"><a href="newpayment.php" class="pure-menu-link">Record Payment</a></li>
 
                 <li class="pure-menu-item" class="menu-item-divided pure-menu-selected">
-                    <a href="#" class="pure-menu-link">About</a>
+                    <a href="logout.php" class="pure-menu-link">Logout</a>
                 </li>
