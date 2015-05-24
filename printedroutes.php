dd1edde14e3cf3d75b9a534ececb1ee331a55398
Axel <axel.hawker@gmail.com>
2015-05-24 15:20:15 -0500

    Cleaned up Route Reporting
    Made Printer-friendly Version

--- printedroutes.php
+++ printedroutes.php
@@ -34,74 +34,8 @@ include "con.php";
     }
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Interface for managing flag subscriptions.">
-
-    <title>Troop833 Flag Subscription Manager</title>
-
-    
-
-
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
-
-
-
-
-  
-    <!--[if lte IE 8]>
-        <link rel="stylesheet" href="css/layouts/side-menu-old-ie.css">
-    <![endif]-->
-    <!--[if gt IE 8]><!-->
-        <link rel="stylesheet" href="css/layouts/side-menu.css">
-    <!--<![endif]-->
-  
-
-
-
-
-    
-
-</head>
-<body>
-
-
-
-
-
-
-<div id="layout">
-    <!-- Menu toggle -->
-    <a href="#menu" id="menuLink" class="menu-link">
-        <!-- Hamburger icon -->
-        <span></span>
-    </a>
-
-    <div id="menu">
-        <div class="pure-menu">
-            <a class="pure-menu-heading" href="#">Troop 833</a>
-
-            <ul class="pure-menu-list">
-                <?php include("menuitems.php"); ?>
-            </ul>
-        </div>
-    </div>
-
-    <div id="main">
-        <div class="header">
-            <h1>Flag Subscription Manager</h1>
-            <h2><?php echo $_SESSION["displayname"]; ?> - View and manage Flag Routes</h2>
-        </div>
-
         <div class="content">
             <h2 class="content-subhead"><?php echo $Route; ?> Route: <?php echo $Count; ?> Flags - Created <?php echo date('l jS \of F Y h:i:s A') ?></h2>
-            <p><form class="pure-form pure-form-stacked" action="printedroutes.php" method="GET">
-                <input type="hidden" value="<?php echo $Route; ?>" name="route">
-                <button type="submit" class="pure-button pure-button-primary">Printer-Friendly Version</button>
-            </form></p>
             
             <table style="width:100%">
 
@@ -124,16 +58,3 @@ include "con.php";
     ?>
                 </table>
         </div>
-    </div>
-</div>
-
-
-
-
-
-<script src="js/ui.js"></script>
-
-
-</body>
-</html>
-
