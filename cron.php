07406eb0ca149e771ab98cf172fcfb6899b92fd6
Axel <axel.hawker@gmail.com>
2015-05-27 11:48:24 -0500

    Added CX E-mail Notifications to Cron Job
    There are 3 notices
    1 - When customers are 30 days or less from expiring
    2 - When customers expire
    3 - When customers status changes to Active (and expdate>now)
    
    The e-mails are send using a function contained in sendmail.php

--- cron.php
+++ cron.php
@@ -1,4 +1,95 @@
 <?php include "conn.php"; 
+include "sendmail.php";
+
+// Notify Customers of Status Changes - Expired
+$ExpiredSuccessCount = 0;
+$ExpiredFailCount = 0;
+
+$Expiredemailsql = "select * from customers WHERE DATEDIFF(`ExpirationDate`,CURDATE()) < 0 and SubStatus != 'Expired'";
+
+$Expiredemailresult = $con->query($Expiredemailsql);
+
+if ($Expiredemailresult->num_rows > 0) {
+    while($expdrow = $Expiredemailresult->fetch_assoc()) {
+        $RecipEmail = $expdrow['Email'];
+        $RecipName = $expdrow['CustomerName'];
+        $Subject = 'Your Flag Service Has Ended - Renew Now';
+        $Body = "Dear " . $expdrow['CustomerName'] . ", <br /> <p>Your Flag Service has ended. Please visit <a href='http://troop833.com/flags'> Our Website </a> To Renew. We appreciate your conintued support.</p> Thanks,<br />Troop 833";
+        $AltBody = 'Your Flag Service has ended. To Renew, please visit http://troop833.com/flags';
+
+        $expiredresponse = FlagMail($RecipEmail, $RecipName, $Subject, $Body, $AltBody);
+
+        if ($expiredresponse == 'Success') {
+            $ExpiredSuccessCount++;
+        }
+        else {
+            $ExpiredFailCount++;
+        }
+    }
+}
+
+echo "Expired Emails Sent: " . $ExpiredSuccessCount . "<br />";
+echo "Expired Emails Failed: " . $ExpiredFailCount . "<br />";
+
+// Notify Customers of Status Changes - Expiring
+$ExpiringSuccessCount = 0;
+$ExpiringFailCount = 0;
+
+$Expiringemailsql = "select * from customers WHERE DATEDIFF(`ExpirationDate`,CURDATE()) between 0 and 30 and SubStatus != 'Expiring Soon'";
+
+$Expiringemailresult = $con->query($Expiringemailsql);
+
+if ($Expiringemailresult->num_rows > 0) {
+    while($expgrow = $Expiringemailresult->fetch_assoc()) {
+        $RecipEmail = $expgrow['Email'];
+        $RecipName = $expgrow['CustomerName'];
+        $Subject = 'Your Flag Service Is Expiring Soon - Renew Now';
+        $Body = 'Your Flag Service ends ' . $expgrow['ExpirationDate'] . '. To Renew, please visit http://troop833.com/flags or reply to this e-mail.';
+        $AltBody = 'Your Flag Service ends ' . $expgrow['ExpirationDate'] . '. To Renew, please visit http://troop833.com/flags or reply to this e-mail.';
+
+
+        $expiringresponse = FlagMail($RecipEmail, $RecipName, $Subject, $Body, $AltBody);
+
+        if ($expiringresponse == 'Success') {
+            $ExpiringSuccessCount++;
+        }
+        else {
+            $ExpiringFailCount++;
+        }
+    }
+}
+
+
+echo "Expiring Soon Emails Sent: " . $ExpiringSuccessCount . "<br />";
+echo "Expiring Soon Emails Failed: " . $ExpiringFailCount . "<br />";
+
+// Notify Customers of Status Changes - Active
+$ActiveSuccessCount = 0;
+$ActiveFailCount = 0;
+$Activeemailsql = "SELECT * from customers WHERE DATEDIFF(`ExpirationDate`,CURDATE()) >= 30 and SubStatus != 'Active'";
+
+$Activeemailresult = $con->query($Activeemailsql);
+
+if ($Activeemailresult->num_rows > 0) {
+    while($activerow = $Activeemailresult->fetch_assoc()) {
+        $RecipEmail = $activerow['Email'];
+        $RecipName = $activerow['CustomerName'];
+        $Subject = 'Your Flag Service Is Now Active - Thanks For Your Support';
+        $Body = "<p>Thank you for your generous support of our Troop. This e-mail is to confirm that your subscription is now active and you will receive service until " . $activerow['ExpirationDate'] . ". </p>Thanks<br />Troop 833";
+        $AltBody = "Thank you for your generous support of our Troop. This e-mail is to confirm that your subscription is now active and you will receive service until " . $activerow['ExpirationDate'] . ". Thanks, Troop 833";
+
+        $activeresponse = FlagMail($RecipEmail, $RecipName, $Subject, $Body, $AltBody);
+
+        if ($activeresponse == 'Success') {
+            $ActiveSuccessCount++;
+        }
+        else {
+            $ActiveFailCount++;
+        }
+    }
+}
+echo "Active Emails Sent: " . $ActiveSuccessCount . "<br />";
+echo "Active Emails Failed: " . $ActiveFailCount . "<br />";
 
 //SQL Queries for Status Management.
 $expiredsql = "UPDATE `customers` SET `Action`='Give Flyer and Attempt Renewal',`SubStatus`='Expired' WHERE DATEDIFF(`ExpirationDate`,CURDATE()) < 0 and SubStatus != 'Expired'";
---
+++ sendmail.php
@@ -0,0 +1,56 @@
+<?php
+//if (!isset($_POST['RecipEmail'] or $_POST['AUTH'] != 'axelssecretpassword') {
+//    exit();
+//}
+//
+////Get e-mail Details
+//    $RecipEmail = $_POST['RecipEmail'];
+//    $RecipName = $_POST['RecipName'];
+//    $Subject = $_POST['Subject'];
+//    $Body = $_POST['Body']; //This is the HTML message body <b>in bold!</b>';
+//    $AltBody = $_POST['AltBody']; //'This is the body in plain text for non-HTML mail clients';
+
+
+require 'vendor/autoload.php';
+
+    
+function FlagMail($RecipEmail, $RecipName, $Subject, $Body, $AltBody) { //
+    
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'axelhawker@gmail.com';                 // SMTP username
+$mail->Password = 'eNPkUc_jJEOYjS3RIPzz5g';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+
+//Set up E-mail
+$mail->From = 'flags@troop833.com';
+$mail->FromName = 'Troop 833 Flag Service';
+$mail->addAddress($RecipEmail, $RecipName);     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo('flags@troop833.com', 'Troop 833 Flag Service');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $Subject;
+$mail->Body    = $Body;
+$mail->AltBody = $AltBody;
+
+if(!$mail->send()) {
+//    echo 'Message could not be sent.';
+    Return('Mailer Error: ' . $mail->ErrorInfo);
+} else {
+    Return('Success');
+}
+}
+    ?>
