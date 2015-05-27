b86315384e68f111f02cea75f34de21ba16f9eb9
Axel <axel.hawker@gmail.com>
2015-05-27 13:05:00 -0500

    Template for Assignment Table
    Created a template for assignment table

--- testing.php
+++ testing.php
@@ -1,1 +1,66 @@
-
+<html>
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc;margin:0px auto;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;}
+@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
+<div class="tg-wrap"><table class="tg">
+  <tr>
+    <th class="tg-031e">Holiday</th>
+    <th class="tg-031e">Route</th>
+    <th class="tg-031e">Setup</th>
+    <th class="tg-031e">TakeDown</th>
+  </tr>
+  <tr>
+    <td class="tg-031e" rowspan="4">Flag Day June 12, 2015</td>
+    <td class="tg-031e">Route1</td>
+    <td class="tg-031e">Set Up Scout 1</td>
+    <td class="tg-031e">Take Down Scout 1</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Route2</td>
+    <td class="tg-031e">Set Up Scout 2</td>
+    <td class="tg-031e">Take Down Scout 2</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Route 3</td>
+    <td class="tg-031e">Set Up Scout 3</td>
+    <td class="tg-031e">Take Down Scout 3</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Route 4</td>
+    <td class="tg-031e">Set Up Scout 4</td>
+    <td class="tg-031e">Take Down Scout 4</td>
+  </tr>
+</table></div>
+<hr>
+<table>
+  <tr>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr>
+    <td rowspan="4"></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+</html>
