5ba768d52077c64f96ad1f10c74cf0ef567df74f
Axel <axel.hawker@gmail.com>
2015-06-23 14:03:23 -0500

    Cleaned up Menu
    If you're not logged in you see very little now

--- menuitems.php
+++ menuitems.php
@@ -1,15 +1,24 @@
+ 
                 <li class="pure-menu-item"><a href="index.php" class="pure-menu-link">Home</a></li>
-                <li class="pure-menu-item"><a href="newpayment.php" class="pure-menu-link">Customers</a></li>
-                <li class="pure-menu-item"><a href="routes.php" class="pure-menu-link">Routes</a></li>
-                <li class="pure-menu-item"><a href="assign.php" class="pure-menu-link">Assignments</a></li>
-                <li class="pure-menu-item"><a href="sales.php" class="pure-menu-link">Sales Tasks</a></li>
-<?php 
+<?php
+if(isset($_SESSION['Username'])){
+                echo '<li class="pure-menu-item"><a href="newpayment.php" class="pure-menu-link">Customers</a></li>';
+                echo '<li class="pure-menu-item"><a href="routes.php" class="pure-menu-link">Routes</a></li>';
+                echo '<li class="pure-menu-item"><a href="assign.php" class="pure-menu-link">Assignments</a></li>';
+                echo '<li class="pure-menu-item"><a href="sales.php" class="pure-menu-link">Sales Tasks</a></li>';
+}
 
 if (isset($_SESSION['role']) and $_SESSION['role'] == 'Admin') { 
     echo "<li class='pure-menu-item'><a href='register.php' class='pure-menu-link'>Users</a>";
     echo "<li class='pure-menu-item'><a href='approvedaddress.php' class='pure-menu-link'>Streets</a>";
 }
 ?>
-                <li class="pure-menu-item"><a href="logout.php" class="pure-menu-link">Logout</a>
-                    
-                </li>
+<?php
+if(isset($_SESSION['Username'])){
+echo '<li class="pure-menu-item"><a href="myaccount.php" class="pure-menu-link">My Account</a></li>';    
+echo '<li class="pure-menu-item"><a href="logout.php" class="pure-menu-link">Logout</a></li>';
+                    }
+else {
+    echo '<li class="pure-menu-item"><a href="#" class="pure-menu-link">Sign In</a></li>';
+}
+                
